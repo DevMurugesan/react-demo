65bdb16ad9006f48bfbd8113f86e435820adaf3f
Murugesan Rethinam <devmurugesanrathinam@gmail.com>
2018-09-08 17:04:59 +0530

    Added actions and reducers

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,11 +1,13 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
 
   constructor(props, context){
     super(props,context);
     this.state = {
-      course: { title: ""}
+      course: { title: "" }
     };
 
     this.onTitleChange = this.onTitleChange.bind(this);
@@ -19,12 +21,17 @@ onTitleChange(event){
 }
 
 onClickSave(){
-  alert('${this.state.course.title}');
+  this.props.dispatch(courseActions.createCourse(this.state.course));
+}
+
+courseRow(course, index) {
+  return <div key={index}>{course.title}</div>;
 }
    render(){
      return(
        <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
         type="text"
@@ -39,4 +46,16 @@ onClickSave(){
      );
    }
 }
-export default CoursesPage;
+
+CoursesPage.PropTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps){
+  return {
+    courses:state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
